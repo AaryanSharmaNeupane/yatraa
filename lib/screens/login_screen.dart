aeac1cd9ff77c6ae719b296efebea2dbeaa726ce
Aaryan Neupane <sonofnaleen@gmail.com>
2023-02-16 15:17:58 +0545

    login and sign up updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:yatraa/helpers/shared_prefs.dart';
 
+import '../widgets/login_buttons.dart';
+import '../widgets/login_label.dart';
+import '../helpers/shared_prefs.dart';
 import '../main.dart';
 import '../screens/home.dart';
 import '../screens/sign_up_screen.dart';
-import '../widgets/bezierContainer.dart';
+import '../widgets/login_headers.dart';
+import '../widgets/login_title.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -18,25 +19,71 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginScreen> {
-  Widget _entryField(String title, {bool isPassword = false}) {
+  String url = "$serverUrl/users/login/";
+  final _formkey = GlobalKey<FormState>();
+  var email = "";
+  var password = "";
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () async {
+        if (_formkey.currentState!.validate()) {
+          setState(
+            () {
+              email = emailController.text;
+              password = passwordController.text;
+            },
+          );
+          final response = await Dio().post(url, data: {
+            "email": email,
+            "password": password,
+          });
+          sharedPreferences.setString("jwt-token", response.data['jwt']);
+          await Dio().get(
+            '$serverUrl/users/',
+            options: Options(
+              headers: {'Cookie': 'jwt=${getToken()}'},
+            ),
+          );
+          // ignore: use_build_context_synchronously
+          Navigator.pushNamedAndRemoveUntil(
+              context, Home.routeName, (route) => false);
+        }
+      },
+      child: button(context, "Login"),
+    );
+  }
+
+  Widget _divider() {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: const <Widget>[
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
+          ),
+          Text("or"),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
           ),
           SizedBox(
-            height: 10,
+            width: 20,
           ),
-          TextField(
-              obscureText: isPassword,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  fillColor: Color(0xfff3f3f4),
-                  filled: true))
         ],
       ),
     );
@@ -44,152 +91,79 @@ class _LoginState extends State<LoginScreen> {
 
   Widget _createAccountLabel() {
     return InkWell(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => SignupScreen()));
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 20),
-        padding: EdgeInsets.all(15),
-        alignment: Alignment.bottomCenter,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Don\'t have an account ?',
-              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SignupScreen()));
+        },
+        child: label(context, 'Don\'t have an account ?', 'Register'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 225, 222, 222),
+      body: Form(
+        key: _formkey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            title(context),
+            header("Email:"),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                  autofocus: false,
+                  decoration: const InputDecoration(
+                    fillColor: Color.fromARGB(255, 201, 201, 201),
+                    border: InputBorder.none,
+                    filled: true,
+                    errorStyle: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 15,
+                    ),
+                  ),
+                  controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'please enter your email';
+                    } else if (!value.contains('@')) {
+                      return 'please enter valid email';
+                    }
+                    return null;
+                  }),
             ),
-            SizedBox(
-              width: 10,
+            header("Password:"),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    fillColor: Color.fromARGB(255, 201, 201, 201),
+                    border: InputBorder.none,
+                    filled: true,
+                    errorStyle: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 15,
+                    ),
+                  ),
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'please enter your password';
+                    }
+                    return null;
+                  }),
             ),
-            Text(
-              'Register',
-              style: TextStyle(
-                  color: Color(0xFF147511),
-                  fontSize: 13,
-                  fontWeight: FontWeight.w600),
+            const SizedBox(
+              height: 10,
             ),
-          ],
+            _submitButton(),
+            _divider(),
+            _createAccountLabel(),
+          ]),
         ),
       ),
     );
-  }
-
-  Widget _title() {
-    return RichText(
-        textAlign: TextAlign.center,
-        text: TextSpan(
-            text: 'YATRAA',
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-                color: Color(0xFF1E671F))));
-  }
-
-  Widget _emailPasswordWidget() {
-    return Column(
-      children: <Widget>[
-        _entryField("Email"),
-        _entryField("Password", isPassword: true),
-      ],
-    );
-  }
-
-  final _formkey = GlobalKey<FormState>();
-  var email = "";
-  var password = "";
-
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  String url = "$serverUrl/users/login/";
-  @override
-  Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-    return Scaffold(
-        backgroundColor: Color(0xFFADCDAD),
-        body: Container(
-          height: height,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                  top: -height * .15,
-                  right: -MediaQuery.of(context).size.width * .4,
-                  child: BezierContainer()),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(height: height * .2),
-                      _title(),
-                      SizedBox(height: 50),
-                      _emailPasswordWidget(),
-                      SizedBox(height: 20),
-                      Container(
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        alignment: Alignment.center,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () async {
-                                if (_formkey.currentState!.validate()) {
-                                  setState(
-                                    () {
-                                      email = emailController.text;
-                                      password = passwordController.text;
-                                    },
-                                  );
-                                  final response = await Dio().post(url, data: {
-                                    "email": email,
-                                    "password": password,
-                                  });
-
-                                  sharedPreferences.setString(
-                                      "jwt-token", response.data['jwt']);
-
-                                  await Dio().get(
-                                    '$serverUrl/users/',
-                                    options: Options(
-                                      headers: {'Cookie': 'jwt=${getToken()}'},
-                                    ),
-                                  );
-
-                                  // ignore: use_build_context_synchronously
-                                  Navigator.pushNamedAndRemoveUntil(context,
-                                      Home.routeName, (route) => false);
-                                }
-                              },
-                              child: const Text(
-                                "Login",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ),
-                            TextButton(
-                                onPressed: () {},
-                                child: const Text(
-                                  'Forgot Password ?',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Color(0xFF0D5116)),
-                                ))
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      _createAccountLabel(),
-                    ],
-                  ),
-                ),
-              ),
-              // Positioned(top: 40, left: 0, child: _backButton()),
-            ],
-          ),
-        ));
   }
 }
